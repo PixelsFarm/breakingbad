79f2120bd93c72295476a3ffd837d8d86848f3b0
hector <h.pascual@ondho.com>
2020-11-23 02:27:58 +0100

    Mostrar una primera frase random al cargar la página con useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Frase from './components/Frase'
 import styled from '@emotion/styled'
 
@@ -10,14 +10,21 @@ const Contenedor = styled.div`
 `
 
 const Boton = styled.button`
-    background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
-    background-size: 300px;
-    font-family: Arial, Helvetica, sans-serif;
-    color: #fff;
-    margin-top: 3rem;
-    padding: 1rem 3rem;
-    font-size: 2rem;border: 2px solid black;
-`
+	background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+	background-size: 300px;
+	font-family: Arial, Helvetica, sans-serif;
+	color: #fff;
+	margin-top: 3rem;
+	padding: 1rem 3rem;
+	font-size: 2rem;
+	border: 2px solid black;
+	transition: background-size 0.8s ease;
+
+	&:hover {
+		cursor: pointer;
+		background-size: 400px;
+	}
+`;
 
 function App() {
     const [frase, guardarFrase] = useState({})
@@ -38,7 +45,12 @@ function App() {
         const api = await fetch("https://breaking-bad-quotes.herokuapp.com/v1/quotes");
         const frase = await api.json();
         guardarFrase(frase[0])
-	};
+    };
+    
+    //* cargar una frase al Ini. UseEffect es como un load o ready
+    useEffect(() => {
+        consultarAPI()
+    }, [])
 
 	return (
 		<Contenedor>
